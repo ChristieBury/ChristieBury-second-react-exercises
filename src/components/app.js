f65723cdead691258ad25c5f8108c2e3d93d871c
makoshika <buryc405@bucs.dawson.edu>
2020-03-28 13:10:40 -0600

    first commit

---
+++ src/components/app.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+export default class App extends Component {
+  render() {
+    return (
+      <div className='app'>
+        <h1>Second React Exercises</h1>
+        
+      </div>
+    );
+  }
+}
